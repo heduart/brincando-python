a9c7e45b2dc7a7197efa704107f31a30ca96dc23
hetduarte <hetduarte@gmail.com>
2021-09-24 15:16:48 -0300

    Criado método para apresentar o ‘id’ criado para um objeto

--- exercicios/pessoa.py
+++ exercicios/pessoa.py
@@ -1,7 +1,14 @@
 """
-Cria uma classe de teste.
+Cria uma classe pessoa para testes.
 """
 
 
 class Pessoa:
-    pass
+    def id_do_objeto(self):
+        # Retornando o Id para um objeto qualquer criado.
+        return id(self)
+
+
+if __name__ == '__main__':
+    pessoa1 = Pessoa()
+    print(f'Retonando o id criado para o objeto: \033[4;33;40m{pessoa1.id_do_objeto()}\033[m')
