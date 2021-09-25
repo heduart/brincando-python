6eb79bd86fe72a768a8f7257277084c669f5835e
hetduarte <hetduarte@gmail.com>
2021-09-24 22:33:26 -0300

    Criado ´__initi__´ e efetuado alguns ajustes no código

--- exercicios/pessoa.py
+++ exercicios/pessoa.py
@@ -4,11 +4,19 @@ Cria uma classe pessoa para testes.
 
 
 class Pessoa:
+    """Criando algumas funções e objetos para a classe pessoa."""
+
+    def __init__(self, nome: str = None, sobrenome: str = None, idade: int = None):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+
     def id_do_objeto(self):
-        # Retornando o Id para um objeto qualquer criado.
+        """Apresentando o ID do objeto criado."""
         return id(self)
 
 
 if __name__ == '__main__':
+    """Testando objetos."""
     pessoa1 = Pessoa()
-    print(f'Retonando o id criado para o objeto: \033[4;33;40m{pessoa1.id_do_objeto()}\033[m')
+    print(f'Retornando o id criado para o objeto: \033[4;31;40m{pessoa1.id_do_objeto()}\033[m')
